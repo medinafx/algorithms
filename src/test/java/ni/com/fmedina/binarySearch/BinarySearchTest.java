70b1bce0e27f8c6d894dfd5b545254c2baa3b64a
medinafx <medina.fx@gmail.com>
2022-07-03 16:42:56 -0600

    Add Squares of Sorted Array

--- src/test/java/ni/com/fmedina/binarySearch/BinarySearchTest.java
+++ src/test/java/ni/com/fmedina/binarySearch/BinarySearchTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BinarySearchTest {
+class BinarySearchTest {
 
     private final BinarySearch binarySearch = new BinarySearch();
 
     @Test
-    public void whenTargetExists_thenReturnIndex() {
+    void whenTargetExists_thenReturnIndex() {
         int[] nums = {1,2,3,4,5,6,7,8,9,10};
         int target = 9, expected = 8;
 
@@ -18,7 +18,7 @@ public class BinarySearchTest {
     }
 
     @Test
-    public void whenTargetNotExists_thenReturnNegativeIndex() {
+    void whenTargetNotExists_thenReturnNegativeIndex() {
         int[] nums = {1,2,3,5,6};
         int target = 7, expected = -1;
 
---
+++ src/test/java/ni/com/fmedina/twoPointers/SquaresOfASortedArrayTest.java
@@ -0,0 +1,21 @@
+package ni.com.fmedina.twoPointers;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.jupiter.api.Test;
+
+class SquaresOfASortedArrayTest {
+
+    SquaresOfASortedArray squaresOfASortedArray = new SquaresOfASortedArray();
+
+    @Test
+    void givenArrayNums_ThenReturnSquaresOfEachNumberInIncreasingOrder() {
+        int[] nums = {-4,-1,0,3,10};
+        int[] expected = {0,1,9,16,100};
+
+        int[] actual = squaresOfASortedArray.sorterdSquares(nums);
+
+        assertThat(expected, is(expected));
+    }
+
+}
