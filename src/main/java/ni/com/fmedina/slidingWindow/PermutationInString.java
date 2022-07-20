a45f36032e8d88ab2a49219689d4fa1645a0f10f
Felix Medina <fmedina@FMEDINA.cetrex.gob.ni>
2022-07-20 14:29:46 -0600

    Fix permutation in string

--- src/main/java/ni/com/fmedina/slidingWindow/PermutationInString.java
+++ src/main/java/ni/com/fmedina/slidingWindow/PermutationInString.java
@@ -28,15 +28,19 @@ public class PermutationInString {
      * @return
      */
     public boolean checkInclusionUsingSort(String s1, String s2) {
-        char[] c = s2.toCharArray();
-        Arrays.sort(c);
-        s2 = new String(c);
-
+        s1 = sort(s1);
         for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            if (s1.equals(s2.substring(i, i + s1.length())))
+            if (s1.equals(sort(s2.substring(i, i + s1.length())))) {
                 return true;
+            }
         }
         return false;
+    }
+
+    private String sort(String s) {
+        char[] c = s.toCharArray();
+        Arrays.sort(c);
+        return new String(c);
     }
 
 }
